c1a487556059210e0179800b48c95c31a1c711e7
longnt <longnt1@kaopiz.com>
2019-09-19 15:56:50 +0700

    Initial commit

---
+++ Assets/Scenes/Scripts/Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    Segment segment;
+    Segment segment2;
+
+    private void Start()
+    {
+        segment = new Segment(0, 0, 0.1f, 45 * Mathf.Deg2Rad);
+        Segment current = segment;
+        for (int i = 0; i < 20; i++) {
+            Segment next = new Segment(current, 0.1f, 0);
+            current.child = next;
+            current = next;
+        }
+    }
+
+    private void Update()
+    {
+        Segment next = segment;
+        while (next != null)
+        {
+            next.Wiggle();
+            next.Update();
+            next = next.child;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Segment next = segment;
+        while (next != null) {
+            next.Draw();
+            next = next.child;
+        }        
+    }
+}
